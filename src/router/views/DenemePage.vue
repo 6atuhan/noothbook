15f9f60b27a204e5be60b932342cffb94cb1a113
6atuhan <boranbatuh@gmail.com>
2022-10-15 18:33:52 +0300

    spline
    spline denemem

--- src/router/views/DenemePage.vue
+++ src/router/views/DenemePage.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    saaaaaaaaaa
+    <iframe src='https://my.spline.design/untitled-1d7b36f4131c03d1c8598378d7b0a7ec/' frameborder='0' width='100%' height='100%'></iframe>
   </div>
 </template>
